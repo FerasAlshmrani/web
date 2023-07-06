f584ecfea2eda81b8ddbc508fd3c4e069a3824af
FerasAlshmrani <froose-999@hotmail.com>
2023-07-06 14:00:37 +0300

    javascript fundmentals till error handling

---
+++ js/js_fundmentals/error_handling.js
@@ -0,0 +1,81 @@
+/*
+Here are some of the most common errors in JavaScript: 
+
+    ReferenceError 
+
+    SyntaxError 
+
+    TypeError 
+
+    RangeError
+
+There are some other errors in JavaScript. These other errors include: 
+
+    AggregateError 
+
+    Error 
+
+    InternalError 
+
+    URIError
+
+
+    -----------------
+    console.log(username); - Uncaught ReferenceError: username is not defined
+
+
+    var a "there's no assignment operator here";  -  Uncaught SyntaxError: Unexpected string
+
+
+    "hello".pop() // Uncaught TypeError: "hello".pop is not a function
+
+
+    (10).toString(100); // Uncaught RangeError: toString() radix argument must be between 2 and 36
+    
+    */
+
+
+    function addTwoNums(a,b){
+        try{
+    
+            if (typeof(a) != 'number'){
+                throw ReferenceError("the first argument is not a number")
+                
+            }
+            else if(typeof(b) != 'number'){
+                throw ReferenceError("the second atgument is not a number")
+                
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+        console.log( a + b )
+    
+    }
+    
+    addTwoNums(5,'5')
+
+// another example :)
+
+    function letterFinder(word, match) {
+        var condition1 = typeof(word) == 'string' && word.length >= 2;
+        var condition2 = typeof(match) == 'string' && match.length == 1;
+        if (condition1 == true && condition2 == true){
+            for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+        }
+        else{
+            console.log("please pass correct arguments to the function.")
+        }
+    
+    }
+
+    letterFinder([],[])
+    letterFinder("cat","c")
---
+++ js/js_fundmentals/string_method.js
@@ -0,0 +1,18 @@
+
+charAt() 
+
+
+// same as =  a + b 
+concat() 
+
+indexOf() 
+
+lastIndexOf() 
+
+
+"ho-ho-ho".split("-"); // ['ho', 'ho', 'ho']
+split() 
+
+toUpperCase() 
+
+toLowerCase()  
